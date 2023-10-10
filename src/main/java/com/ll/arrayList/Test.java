d24288efb9342a6884fc11718e9e78c8f6b9e461
yoodongan <dbehddks519@naver.com>
2023-10-10 20:51:47 +0900

    add NUMBER

--- src/main/java/com/ll/arrayList/Test.java
+++ src/main/java/com/ll/arrayList/Test.java
@@ -1,6 +1,7 @@
 package com.ll.arrayList;
 
 public class Test {
+    private static final int NUMBER = 10;
     public static void main(String[] args) {
         System.out.println("test code");
     }
